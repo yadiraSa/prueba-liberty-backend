59d89951e8a1aa4e497e3ad9426920228e1fbe72
Yadira Salazar <yadira.salazar@clickittech.com.mx>
2023-10-10 18:04:16 -0600

    add: :sparkles: server functionality

--- README.md
+++ README.md
@@ -1,9 +1,19 @@
 # Description
-This a the technical test for the fullstack position 
+
+This a the technical test for the fullstack position
+
 # Getting Started
+
 Run `npm i`
+
 # Run de project on the terminal
+
 Go to the folder `server`
 Open `server.js` file
 Write the command `node server.js`
+
 # NODEJS
+
+# Nombre de la candidata
+
+Yadira Alejandrina Salazar Alvarez
--- server/server.js
+++ server/server.js
@@ -1,48 +1,50 @@
-const express = require('express');
-const cors = require('cors');
-const morgan = require('morgan');
+const express = require("express");
+const cors = require("cors");
+const morgan = require("morgan");
 
 const app = express();
 const PORT = process.env.PORT || 3001;
 
 app.use(cors());
-app.use(morgan('tiny'));
+app.use(morgan("tiny"));
 
 const estadosMexicanos = [
-  { id: 1, nombre: 'Ciudad de México' },
-  { id: 2, nombre: 'Monterrey Nuevo León' },
-  { id: 3, nombre: 'Saltillo, Coahuila' },
+  { id: 1, nombre: "Ciudad de México", fianza: "1954-0000" },
+  { id: 2, nombre: "Monterrey, Nuevo León", fianza: "8524-0000" },
+  { id: 3, nombre: "Saltillo, Coahuila", fianza: "1584-0000" },
+  { id: 4, nombre: "Aguascalientes", fianza: "1845-0000" },
+  { id: 5, nombre: "Querétaro", fianza: "9711-0457" },
+  { id: 6, nombre: "Guadalajara, Jal", fianza: "6857-0457" },
 ];
 
 const agentes = [
-  { id: 1, nombre: 'Zurich'},
-  { id: 2, nombre: 'MAPFRE'},
-  { id: 3, nombre: 'GNP'},
-  { id: 4, nombre: 'METLIFE'},
-  { id: 5, nombre: 'ALLIANZ'},
-]
+  { id: 1, nombre: "Zurich" },
+  { id: 2, nombre: "MAPFRE" },
+  { id: 3, nombre: "GNP" },
+  { id: 4, nombre: "METLIFE" },
+  { id: 5, nombre: "ALLIANZ" },
+];
 
 const ejecutivos = [
-  { id: 1, nombre: 'Mario Estrada'},
-  { id: 2, nombre: 'Carla Pereira'},
-  { id: 3, nombre: 'Mariana Torres'},
-  { id: 4, nombre: 'Cesar Prado'},
-  { id: 5, nombre: 'Adrián Mendoza'},
-]
+  { id: 1, nombre: "Mario Estrada" },
+  { id: 2, nombre: "Carla Pereira" },
+  { id: 3, nombre: "Mariana Torres" },
+  { id: 4, nombre: "Cesar Prado" },
+  { id: 5, nombre: "Adrián Mendoza" },
+];
 
 // Ruta para obtener la lista de estados
-app.get('/estados', (req, res) => {
+app.get("/estados", (req, res) => {
   res.json(estadosMexicanos);
 });
 
-app.get('/agentes', (req, res) => {
+app.get("/agentes", (req, res) => {
   res.json(agentes);
 });
 
-app.get('/ejecutivos', (req, res) => {
+app.get("/ejecutivos", (req, res) => {
   res.json(ejecutivos);
 });
-
 
 app.listen(PORT, () => {
   console.log(`Servidor API en ejecución en el puerto ${PORT}`);
